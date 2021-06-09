418c50bcbddc1d303c261659962c97159238d22b
Mikael Amaral <55303570+Mikaral@users.noreply.github.com>
2021-06-09 17:04:17 -0300

    Adding enemies and movement mechanics

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 # Initialize pygame
@@ -16,9 +17,19 @@ playerImg = pygame.image.load('space-invaders.png')
 playerX = 368
 playerY = 480
 
+# Enemy
+enemyImg = pygame.image.load('rodrigo.png')
+enemyX = random.randint(0, 800 - 64)
+enemyY = random.randint(50, 150)
+enemyDeltaMovement = 0.1
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # Game Loop
@@ -27,13 +38,31 @@ while running:
     # Setting background color to white
     screen.fill((0, 0, 0))
 
+    # Vector of all inputs
+    pressed = pygame.key.get_pressed()
+
+    # Movement of the ship
+    if pressed[pygame.K_LEFT] and playerX > 0:
+        playerX -= 0.2
+    elif pressed[pygame.K_RIGHT] and playerX < 800 - 64:
+        playerX += 0.2
+
+    # Movement of the Enemy
+    if enemyX >= 800 - 64:
+        enemyDeltaMovement = -0.1
+    elif enemyX <= 0:
+        enemyDeltaMovement = 0.1
+
+    enemyX += enemyDeltaMovement
+    enemyY += 0.001
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Drawing Player Model
-    player()
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
 
     # Updating screen
     pygame.display.update()
-
